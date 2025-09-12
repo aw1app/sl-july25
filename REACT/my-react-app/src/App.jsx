8f2d7acda9a152b56028fb542a28179dbc9e7647
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-09-12 07:58:52 +0530

    Course 3 react routing demo 4

--- REACT/my-react-app/src/App.jsx
+++ REACT/my-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import UserProfile from './pages/UserProfile';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Dashboard from './pages/Dashboard';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
     return (
@@ -13,6 +14,8 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/user/:userId/:x1" element={<UserProfile />} />
                 <Route path="/dashboard" element={<Dashboard />} />
+
+                <Route path="/*" element={<NotFoundPage />} />
             </Routes>
             <Footer />
      </>
@@ -20,3 +23,4 @@ function App() {
 }
 
 export default App;
+
---
+++ REACT/my-react-app/src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>404 Not Found</h1>
+            <p>The page you are looking for doesn't exist.</p>
+        </div>
+    );
+};
+export default NotFoundPage;
