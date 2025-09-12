d86a48d91a621c6a5ecd1ae04bde5114b9f2b615
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-09-12 07:20:46 +0530

    Course 3 react routing demo 3

--- REACT/my-react-app/src/App.jsx
+++ REACT/my-react-app/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/user/:userId" element={<UserProfile />} />
+                <Route path="/user/:userId/:x1" element={<UserProfile />} />
             </Routes>
             <Footer />
      </>
--- REACT/my-react-app/src/pages/UserProfile.jsx
+++ REACT/my-react-app/src/pages/UserProfile.jsx
@@ -1,14 +1,15 @@
 import { useParams } from 'react-router-dom';
 
 function UserProfile() {
-    const { userId } = useParams();
+    const { userId, x1 } = useParams();
 
-    
+
 
     return (
         <div>
             <h2>User Profile</h2>
             <p>User ID: {userId}</p>
+            <p>User ID: {x1}</p>
         </div>
     );
 }
